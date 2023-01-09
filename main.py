e3442d01d5910c044382e6db18cc9530ef0da40d
nnox <noxormy@gmail.com>
2023-01-09 17:57:00 +0400

    add cors for local requests

--- main.py
+++ main.py
@@ -5,9 +5,9 @@
 
 import strawberry
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from strawberry.asgi import GraphQL
 from ql_types import Query
-
 
 schema = strawberry.Schema(query=Query)
 graphql_app = GraphQL(schema)
@@ -15,3 +15,13 @@ graphql_app = GraphQL(schema)
 app = FastAPI()
 app.add_route("/graphql", graphql_app)
 app.add_websocket_route("/graphql", graphql_app)
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
